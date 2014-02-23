b55faf16afc044629a213e82690a9cc633734b36
Sun Zheng'an <sun-zheng-an@users.noreply.github.com>
2014-02-23 22:46:57 +0800

    Remove support for running multiple commands

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var _       = require('lodash')
-var async   = require('async')
 var cp      = require('child_process')
 var gutil   = require('gulp-util')
 var join    = require('path').join
@@ -7,7 +6,7 @@ var through = require('through2')
 
 var PLUGIN_NAME = 'gulp-shell'
 
-function shell(commands, options) {
+function shell(command, options) {
   if (!options) options = {}
   var ignoreErrors = !!options.ignoreErrors
   var quiet        = !!options.quiet
@@ -18,33 +17,27 @@ function shell(commands, options) {
   var env = _.extend({}, process.env, {PATH: path})
 
   return through.obj(function (file, _, done) {
-    var self = this
+    command = gutil.template(command, {file: file})
 
-    async.eachSeries(commands, function (command, done) {
-      command = gutil.template(command, {file: file})
+    cp.exec(command, {env: env}, function (error, stdout, stderr) {
+      if (!quiet) {
+        if (stderr) gutil.log(stderr.trim())
+        if (stdout) gutil.log(stdout.trim())
+      }
 
-      cp.exec(command, {env: env}, function (error, stdout, stderr) {
-        if (!quiet) {
-          if (stderr) gutil.log(stderr.trim())
-          if (stdout) gutil.log(stdout.trim())
-        }
-
-        done(ignoreErrors ? null : error)
-      })
-    }, function (error) {
-      if (error) {
-        self.emit('error', new gutil.PluginError(PLUGIN_NAME, error))
+      if (error && !ignoreErrors) {
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME, error))
       } else {
-        self.push(file)
+        this.push(file)
       }
       done()
-    })
+    }.bind(this))
   })
 }
 
-shell.task = function (commands, options) {
+shell.task = function (command, options) {
   return function () {
-    var stream = shell(commands, options)
+    var stream = shell(command, options)
 
     stream.write(new gutil.File())
     stream.end()
