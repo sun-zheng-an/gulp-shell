01c09583292e9ed3662422277032fc15d7fa5ac7
Sun Zheng'an <sun-zheng-an@users.noreply.github.com>
2014-02-26 12:27:40 +0800

    Use jscs to check style

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,10 @@ gulp.task('coverage', ['test'], shell.task('istanbul cover _mocha -- -R spec'))
 
 gulp.task('coveralls', ['coverage'], shell.task('cat coverage/lcov.info | coveralls'))
 
-gulp.task('lint', shell.task('jshint ' + paths.js.join(' ')))
+gulp.task('lint', shell.task([
+  'jshint ' + paths.js.join(' '),
+  'jscs '   + paths.js.join(' '),
+]))
 
 gulp.task('default', ['coverage', 'lint'])
 
