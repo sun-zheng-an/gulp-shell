8b0e0709145dd2cb7f209e98b156f01a3132e848
light <light-li@hotmail.com>
2014-07-11 23:39:09 +0800

    fixed bug

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var _       = require('lodash')
 var async   = require('async')
 var exec    = require('child_process').exec
 var gutil   = require('gulp-util')
-var path    = require('path')
-var join    = path.join
+var pathMod = require('path')
+var join    = pathMod.join
 var through = require('through2')
 
 var PLUGIN_NAME = 'gulp-shell'
@@ -25,7 +25,7 @@ function shell(commands, options) {
   }, options)
 
   var pathToBin = join(process.cwd(), 'node_modules/.bin')
-  var separator = path.delimiter
+  var separator = pathMod.delimiter
   var path = pathToBin + separator + process.env.PATH
   var env = _.defaults({PATH: path}, process.env)
 
